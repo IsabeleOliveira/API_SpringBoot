798cfa3e556bd804231df6e3ea5a9a17a8ca7726
Isa_oliveira <belefreiresls@outloo.com>
2022-10-26 14:26:04 -0300

    Alterando para Postgress

--- src/main/java/com/alga/algacrmapi/controllers/ClienteController.java
+++ src/main/java/com/alga/algacrmapi/controllers/ClienteController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "https://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/clientes")
 public class ClienteController {
 
