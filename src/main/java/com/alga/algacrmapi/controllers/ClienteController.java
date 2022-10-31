59069f3eab8b968d2851b701dc22149072edc7aa
Isa_oliveira <belefreiresls@outloo.com>
2022-10-31 19:01:18 -0300

    Deletar Cliente

--- src/main/java/com/alga/algacrmapi/controllers/ClienteController.java
+++ src/main/java/com/alga/algacrmapi/controllers/ClienteController.java
@@ -5,6 +5,7 @@ import com.alga.algacrmapi.repositories.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -12,13 +13,20 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/clientes")
 public class ClienteController {
-
     @Autowired
     private ClienteRepository clienteRepository;
 
     @GetMapping
     public List<Cliente> listar(){
         return clienteRepository.findAll();
+    }
+    @GetMapping("/{id}")
+    public Cliente buscarPorId(@PathVariable Long id) {
+        var clienteOptional = clienteRepository.findById(id);
+        if (clienteOptional.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return clienteOptional.get();
     }
 
     @PostMapping
@@ -27,4 +35,13 @@ public class ClienteController {
         return clienteRepository.save(cliente);
     }
 
+    @DeleteMapping("/{id}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void excluirPorId(@PathVariable Long id) {
+        var clienteOptional = clienteRepository.findById(id);
+        if (clienteOptional.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        clienteRepository.delete(clienteOptional.get());
+    }
 }
